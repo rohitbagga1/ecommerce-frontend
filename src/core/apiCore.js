4f252e127cf2b2a9595b64ba1f57e63c69894793
rohit <you@example.com>
2023-11-11 23:50:31 +0530

    processing payment done

--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -80,9 +80,25 @@ export const getBraintreeClientToken = (userId, token) => {
         method: "GET",
         headers: {
             Accept: "application/json",
-            "Content-Type": "application/jsostart-nodmonn",
+            "Content-Type": "application/json",
             Authorization: `Bearer ${token}`
         }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const processPayment = (userId, token, paymentData) => {
+    return fetch(`${API}/braintree/payment/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(paymentData)
     })
         .then(response => {
             return response.json();
